16e34c749babe0426c78f1dceffeeb685c7ae5b7
Cerebus451 <cerebus451@gmail.com>
2016-03-25 16:01:01 +0000

    Added user/article association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,10 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    # debugger => Adding in this line will stop the processing of the page and starts a debugger console where the rails server is running
     #render plain: params[:article].inspect => display what was sent on the screen
     @article = Article.new(article_params)
+    @article.user = User.first # temporary until log in is implemented
     if @article.save
       flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
