89184babd6acf8b5b2f5040624c8bc4ba3a6fbd4
Cerebus451 <cerebus451@gmail.com>
2016-03-26 23:54:58 +0000

    Wrap up categories and initial coding

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   
-  before_action :require_admin, only: [:new, :create]
+  before_action :require_admin, only: [:new, :create, :edit, :update]
+  before_action :get_category, only: [:show, :edit, :update]
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -11,7 +12,6 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -25,7 +25,23 @@ class CategoriesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category successfully renamed"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+  
   private
+  
+  def get_category
+    @category = Category.find(params[:id])
+  end
   
   def category_params
     params.require(:category).permit(:name)
@@ -33,7 +49,7 @@ class CategoriesController < ApplicationController
   
   def require_admin
     if !logged_in? || !current_user.is_admin?
-      flash[:danger] = "You must be an administrator to create categories"
+      flash[:danger] = "You must be an administrator to create/modify categories"
       redirect_to categories_path
     end
   end
